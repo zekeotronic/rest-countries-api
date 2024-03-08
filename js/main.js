2a612d4308d436014a45abca735a4dc6fd6b2101
Eric Ihle <zekeotronic@gmail.com>
2024-03-07 20:48:53 -0500

    adds svg icons, finishes dark/light mode, starts pulling in API data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,90 @@
-console.log('Hello, World!')
+const themeChooser = document.getElementById('theme-chooser');
+const theme = document.getElementById('theme');
+const themeText = document.getElementById('theme-text');
+const themeIcon = document.getElementById('theme-icon');
+const searchIcon = document.getElementById('search-icon');
+const countries = document.getElementById('countries-wrapper');
+const darkMode = './css/dark.css';
+const lightMode = './css/light.css';
+const moon = './assets/moon.svg';
+const sun = './assets/sun.svg';
+
+const icons = {
+  back: {
+    light: './assets/back.svg',
+    dark: './assets/back-dark.svg',
+  },
+  down: {
+    light: './assets/down.svg',
+    dark: './assets/down-dark.svg',
+  },
+  up: {
+    light: './assets/up.svg',
+    dark: './assets/up-dark.svg',
+  },
+  search: {
+    light: './assets/search.svg',
+    dark: './assets/search-dark.svg',
+  }
+};
+
+// checkForDarkMode();
+themeChooser.addEventListener('click', toggleDarkMode);
+
+function toggleDarkMode() {
+  if (theme.getAttribute('href') ===  darkMode) {
+    theme.href =  lightMode;
+    themeText.textContent = 'Dark Mode';
+    themeIcon.src = moon
+    searchIcon.src = icons.search.light;
+  } else {
+    theme.href = darkMode;
+    themeText.textContent = 'Light Mode';
+    themeIcon.src = sun
+    searchIcon.src = icons.search.dark;
+  }
+}
+
+function checkForDarkMode() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    toggleDarkMode()
+  }
+}
+
+function createCountyCard(country) {
+  const card = document.createElement('div');
+  card.classList.add('country-card');
+  card.innerHTML = `
+    <div class="flag">
+      <img src="${country.flags.svg}" alt="${country.flag.alt}">
+    </div>
+    <div class="country-info">
+      <h2>${country.name.common}</h2>
+      <p><strong>Population:</strong> ${country.population.toLocaleString('en-us')}</p>
+      <p><strong>Region:</strong> ${country.region}</p>
+      <p><strong>Capital:</strong> ${country.capital}</p>
+    </div>
+  `;
+  return card;
+
+}
+
+async function getAllCountries() {
+  const response = await fetch('https://restcountries.com/v3.1/all');
+  const data = await response.json();
+  return data;
+}
+
+async function getCountriesByRegion(region) {
+  const response = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+  const data = await response.json();
+  return data;
+}
+
+
+
+getAllCountries().then(data => {
+  data.forEach(country => {
+    countries.appendChild(createCountyCard(country));
+  });
+  })
